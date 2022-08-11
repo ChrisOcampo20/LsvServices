b4fca57cd22703ec2b5dd7d3e68bb0078afac038
ChrisOcampo20 <chrisocampo177@gmail.com>
2022-08-11 17:08:21 -0500

    Initial commit

---
+++ Assets/Scripts/Camara.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camara : MonoBehaviour
+{
+    private new Transform camera;
+    public Vector2 sensibility;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = transform.Find("Camera");
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hor = Input.GetAxis("Mouse X");
+        float ver = Input.GetAxis("Mouse Y");
+
+        if(hor !=0)
+        {
+            transform.Rotate(Vector3.up * hor * sensibility.x);
+        }
+
+        if(ver != 0) 
+        {
+            //camera.Rotate(Vector3.left * ver * sensibility.y);
+            float angle = (camera.localEulerAngles.x - ver * sensibility.y + 360) % 360;
+            if (angle > 180) { angle -= 360; }
+            angle = Mathf.Clamp(angle, -80, 80);
+
+            camera.localEulerAngles = Vector3.right * angle;
+        }
+
+        
+
+    }
+}
